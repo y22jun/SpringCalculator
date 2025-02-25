15c31cf097ae434ce9bfa19d032351287df76a7d
y22jun <tlsdpwns0612@naver.com>
2025-02-25 16:15:23 +0900

    Refactor:Operator class enum으로 변경.

--- src/operator/Operator.java
+++ src/operator/Operator.java
@@ -1,5 +1,31 @@
 package operator;
 
-public interface Operator {
-    double result(double a, double b);
+import static exception.ErrorMessage.DIVIDE_NOT_ZERO;
+
+public enum Operator {
+
+    PLUS {
+        public double result(double a, double b) {
+            return a + b;
+        }
+    },
+    MINUS {
+        public double result(double a, double b) {
+            return a - b;
+        }
+    },
+    MULTIPLY {
+        public double result(double a, double b) {
+            return a * b;
+        }
+    },
+    DIVIDE {
+        public double result(double a, double b) {
+            if(b == 0) {
+                throw new IllegalArgumentException(DIVIDE_NOT_ZERO.getMessage());
+            }
+            return a / b;
+        }
+    };
+    public abstract double result(double a, double b);
 }

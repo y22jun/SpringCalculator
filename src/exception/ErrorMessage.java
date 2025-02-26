5a787898d61647daeaa99b04397cb3433ea2f4e7
y22jun <tlsdpwns0612@naver.com>
2025-02-26 16:51:56 +0900

    Refactor:입력값에 공백 검증 에러메시지 추가

--- src/exception/ErrorMessage.java
+++ src/exception/ErrorMessage.java
@@ -7,6 +7,7 @@ public enum ErrorMessage {
     INVALID_SEPARATOR("유효하지 않은 구분자입니다."),
     INVALID_OPERATE("유효하지 않은 사칙연산자입니다."),
     MISSING_WHITESPACE_BEFORE_OPERATOR("연산자 앞은 공백이어야 합니다."),
+    INPUT_WHITESPACE("입력값에 공백이 있으면 안됩니다."),
     DIVIDE_NOT_ZERO("0으로 나눌 수 없습니다."),
     ;
 

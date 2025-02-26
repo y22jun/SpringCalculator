814277bf307255571052b070a09712f54f9f47f7
y22jun <tlsdpwns0612@naver.com>
2025-02-26 16:32:40 +0900

    CalculatorResult 클래스에서 계산결과 반환.

--- src/output/CalculatorResult.java
+++ src/output/CalculatorResult.java
@@ -4,4 +4,7 @@ import java.util.List;
 
 public record CalculatorResult(List<Double> numbers, char operator, double result) {
 
+    public String inputResult() {
+        return String.format("%.1f", result);
+    }
 }

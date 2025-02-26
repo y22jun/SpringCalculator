3024241c4a754f8223e24c2f56772e3a8336a2f0
y22jun <tlsdpwns0612@naver.com>
2025-02-26 16:35:45 +0900

    CalculatorResult 클래스에서 수식결과 반환.

--- src/output/CalculatorResult.java
+++ src/output/CalculatorResult.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 public record CalculatorResult(List<Double> numbers, char operator, double result) {
 
+    public String expressionResult() {
+        StringBuilder expressionResult = new StringBuilder();
+        expressionResult.append(numbers().get(0));
+        for(int i = 1; i < numbers().size(); i++) {
+            expressionResult.append(" ").append(operator()).append(" ").append(numbers().get(i));
+        }
+
+        return expressionResult.toString();
+    }
+
     public String inputResult() {
         return String.format("%.1f", result);
     }

85cd45f8519f996dcb7a05df19193ee4e85663ea
y22jun <tlsdpwns0612@naver.com>
2025-02-27 21:49:55 +0900

    Chore:CalculatorResult class 매직넘버 수정.

--- src/output/CalculatorResult.java
+++ src/output/CalculatorResult.java
@@ -5,9 +5,11 @@ import java.util.List;
 public record CalculatorResult(List<Double> numbers, char operator, double result) {
 
     public String expressionResult() {
+        final int NUMBER_SIZE = numbers().size();
+        final double FIRST_INDEX = numbers().get(0);
         StringBuilder expressionResult = new StringBuilder();
-        expressionResult.append(numbers().get(0)); //numbers().get(0) 수정
-        for(int i = 1; i < numbers().size(); i++) {
+        expressionResult.append(FIRST_INDEX);
+        for(int i = 1; i < NUMBER_SIZE; i++) {
             expressionResult.append(" ").append(operator()).append(" ").append(numbers().get(i)); //분리를 해서 더 단순하게 만들자
         }
 

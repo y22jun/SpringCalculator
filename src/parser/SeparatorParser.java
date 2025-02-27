20f97610a5c80ed13f586e248ee20e547e38600c
y22jun <tlsdpwns0612@naver.com>
2025-02-27 21:47:01 +0900

    Chore:SeparatorParser class 매직넘버 수정.

--- src/parser/SeparatorParser.java
+++ src/parser/SeparatorParser.java
@@ -12,8 +12,9 @@ public class SeparatorParser {
     public List<Double> parserSeparator(String input) {
         List<Double> result = new ArrayList<>();
         StringBuilder number = new StringBuilder();
+        final int BEFORE_WHITESPACE_INDEX = input.length() - 2;
 
-        for(int i = 0; i < input.length() - 2; i++) {
+        for(int i = 0; i < BEFORE_WHITESPACE_INDEX; i++) {
             appendNumber(input.charAt(i), number, result);
         }
 
